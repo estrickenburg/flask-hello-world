509e420d790c98dbe7961f05e3133b3717b47697
Elizabeeth Strickenburg <strickenburgpiano@gmail.com>
2023-03-25 20:14:32 +0000

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a/basketball_db")
+    conn.close()
+    return "Database Connection Successful"
