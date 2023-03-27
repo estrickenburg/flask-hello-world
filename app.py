8481d71908ee27acb38c51a4270f1b3d3f90f332
Elizabeeth Strickenburg <strickenburgpiano@gmail.com>
2023-03-27 17:04:03 +0000

    create route

--- app.py
+++ app.py
@@ -18,60 +18,60 @@ def creating():
     conn = psycopg2.connect("postgresql://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a.ohio-postgres.render.com/basketball_db")
     cur = conn.cursor()
     cur.execute('''
-        CREATE TABLE IF NOT EXISTS Basketball (
+        CREATE TABLE IF NOT EXISTS Basketball(
         First varchar(255),
         Last varchar(255),
         City varchar(255),
         Name varchar(255),
         Number int
         );
-        ''')
+    ''')
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
 
-@app.route('/db_insert')
-def inserting():
-    conn = psycopg2.connect("postgresql://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a.ohio-postgres.render.com/basketball_db")
-    cur = conn.cursor()
-    cur.execute('''
-        INSERT INTO Basketball (First, Last, City, Name, Number)
-        Values
-        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
-        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
-        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
-        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
-        ''')
-    conn.commit()
-    conn.close()
-    return "Basketball Table Successfully Populated"
+# @app.route('/db_insert')
+# def inserting():
+#     conn = psycopg2.connect("postgresql://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a.ohio-postgres.render.com/basketball_db")
+#     cur = conn.cursor()
+#     cur.execute('''
+#         INSERT INTO Basketball (First, Last, City, Name, Number)
+#         Values
+#         ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+#         ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+#         ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+#         ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+#         ''')
+#     conn.commit()
+#     conn.close()
+#     return "Basketball Table Successfully Populated"
 
-@app.route('/db_drop')
-def dropping():
-    conn = psycopg2.connect("postgresql://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a.ohio-postgres.render.com/basketball_db")
-    cur = conn.cursor()
-    cur.execute('''
-        DROP TABLE Basketball;
-        ''')
-    conn.commit()
-    conn.close()
-    return "Basketball Table Successfully Dropped"
+# @app.route('/db_drop')
+# def dropping():
+#     conn = psycopg2.connect("postgresql://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a.ohio-postgres.render.com/basketball_db")
+#     cur = conn.cursor()
+#     cur.execute('''
+#         DROP TABLE Basketball;
+#         ''')
+#     conn.commit()
+#     conn.close()
+#     return "Basketball Table Successfully Dropped"
 
-@app.route('/db_select')
-def selecting():
-    conn = psycopg2.connect("postgresql://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a.ohio-postgres.render.com/basketball_db")
-    cur = conn.cursor()
-    cur.execute('''
-        SELECT * FROM Basketball;
-        ''')
-    records = cur.fetchall()
-    conn.close()
-    response_string=""
-    response_string+="<table>"
-    for player in records:
-        response_string+="<tr>"
-        for info in player:
-            response_string+="<td>{}</td>".format(info)
-        response_string+="</tr>"
-    response_string+="</table>"
-    return response_string
+# @app.route('/db_select')
+# def selecting():
+#     conn = psycopg2.connect("postgresql://basketball_db_user:2AotULLsHGewiAHNHCxZRgoU1OafHJpI@dpg-cgfku182qv28tc05hmj0-a.ohio-postgres.render.com/basketball_db")
+#     cur = conn.cursor()
+#     cur.execute('''
+#         SELECT * FROM Basketball;
+#         ''')
+#     records = cur.fetchall()
+#     conn.close()
+#     response_string=""
+#     response_string+="<table>"
+#     for player in records:
+#         response_string+="<tr>"
+#         for info in player:
+#             response_string+="<td>{}</td>".format(info)
+#         response_string+="</tr>"
+#     response_string+="</table>"
+#     return response_string
